469eea6fe1d9a924e73f55c7befa3985b54b3db7
Jared Bash <jbash94@gmail.com>
2022-08-29 15:25:37 +0200

    general commit

---
+++ frontend/src/AutoComplete.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+
+const AutoComplete = () => (
+    <div>
+        <GooglePlacesAutocomplete />
+    </div>
+);
+
+export default AutoComplete;
